74fe1dbdd516a8f8e8317aa2c7e9da2fbf95631f
olegk <oligkondr@mail.ru>
2024-06-29 10:43:07 +0300

    fix workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import path from 'node:path';
 
 export default defineConfig({
     plugins: [
@@ -12,9 +11,4 @@ export default defineConfig({
         }),
         react(),
     ],
-    resolve: {
-        alias: {
-            'ziggy-js': path.resolve('vendor/tightenco/ziggy/dist'),
-        },
-    },
 });
